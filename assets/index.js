f66f84bb7f2c984a84cfef1975ca3a4c96ece097
Rodrigo Ramírez <yitodeveloper@gmail.com>
2020-03-02 14:27:51 -0300

    manejo de plugins

--- assets/index.js
+++ assets/index.js
@@ -1,26 +1,11 @@
+import MediaPlayer from './MediaPlayer.js'
+import AutoPlay from './plugins/AutoPlay.js'
+
 const video = document.querySelector("video");
 const button = document.querySelector("button");
 
-function MediaPlayer(config){
-    this.media = config.el;
-}
-
-MediaPlayer.prototype.play = function(){
-    this.media.play()
-}
-
-MediaPlayer.prototype.pause = function(){
-    this.media.pause()
-}
-
-MediaPlayer.prototype.togglePlay = function(){
-    if(this.media.paused){
-        this.media.play()
-    }else{
-        this.media.pause()
-    }
-}
-
-const player = new MediaPlayer({ el: video });
+const player = new MediaPlayer({ el: video, plugins: [
+    new AutoPlay()
+] });
 
 button.onclick = () => player.togglePlay();
